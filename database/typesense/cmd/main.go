41abacc4fa13cf88e60078dcbb4c4a1ca7a05a65
Jonathon Beauregard II <jonathon@floatplanemedia.com>
2026-05-20 09:37:57 -0700

    database/typesense: simplify entry point in main.go
    Signed-off-by: Jonathon Beauregard II <jonathon@floatplanemedia.com>

--- database/typesense/cmd/main.go
+++ database/typesense/cmd/main.go
@@ -4,18 +4,11 @@
 package main
 
 import (
-	"os"
-
 	"github.com/openbao/openbao-plugins/database/typesense"
-	"github.com/openbao/openbao/api/v2"
 	dbplugin "github.com/openbao/openbao/sdk/v2/database/dbplugin/v5"
 )
 
 func main() {
-	apiClientMeta := &api.PluginAPIClientMeta{}
-	flags := apiClientMeta.FlagSet()
-	flags.Parse(os.Args[1:])
-
-	// ServeMultiplex directly takes the factory function in the v5 interface
 	dbplugin.ServeMultiplex(typesense.New)
 }
+
